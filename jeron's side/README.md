0044d38c279c8d91f97d5f977a86002c0619fbe8
nwqj22 <65044854+nwqj22@users.noreply.github.com>
2024-03-22 09:44:17 +0800

    testing phase

---
+++ jeron's side/README.md
@@ -0,0 +1,12 @@
+IOT README LINKS
+https://www.tim-kleyersburg.de/articles/home-assistant-with-docker-2023/
+https://www.homeautomationguy.io/blog/home-assistant-tips/installing-docker-home-assistant-and-portainer-on-ubuntu-linux
+https://www.homeautomationguy.io/blog/docker-tips/configuring-the-mosquitto-mqtt-docker-container-for-use-with-home-assistant
+
+Credentials
+HA – nwqj:p@ssw0rd
+Portcontainer – admin:p@ssw0rd1234
+MQTT – mqtt_user:p@ssw0rd
+
+
+idf.py -p COM6 -b 115200 flash
---
+++ jeron's side/df_client/build/bootloader-prefix/tmp/bootloader-mkdirs.cmake
@@ -0,0 +1,22 @@
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file Copyright.txt or https://cmake.org/licensing for details.
+
+cmake_minimum_required(VERSION 3.5)
+
+file(MAKE_DIRECTORY
+  "C:/Users/Jeron/esp/v5.2.1/esp-idf/components/bootloader/subproject"
+  "E:/Work/CSC2106/jeron's side/df_client/build/bootloader"
+  "E:/Work/CSC2106/jeron's side/df_client/build/bootloader-prefix"
+  "E:/Work/CSC2106/jeron's side/df_client/build/bootloader-prefix/tmp"
+  "E:/Work/CSC2106/jeron's side/df_client/build/bootloader-prefix/src/bootloader-stamp"
+  "E:/Work/CSC2106/jeron's side/df_client/build/bootloader-prefix/src"
+  "E:/Work/CSC2106/jeron's side/df_client/build/bootloader-prefix/src/bootloader-stamp"
+)
+
+set(configSubDirs )
+foreach(subDir IN LISTS configSubDirs)
+    file(MAKE_DIRECTORY "E:/Work/CSC2106/jeron's side/df_client/build/bootloader-prefix/src/bootloader-stamp/${subDir}")
+endforeach()
+if(cfgdir)
+  file(MAKE_DIRECTORY "E:/Work/CSC2106/jeron's side/df_client/build/bootloader-prefix/src/bootloader-stamp${cfgdir}") # cfgdir has leading slash
+endif()
