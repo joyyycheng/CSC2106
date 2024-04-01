9a8a189b85226ba437394283d36ddb8fc23819b5
Mark <14786851+memersonloh@users.noreply.github.com>
2024-04-01 19:35:39 +0800

    Create falldetection-mark.ino

---
+++ falldetection/falldetection-mark.ino
@@ -0,0 +1,344 @@
+#include <painlessMesh.h>
+#include <M5StickCPlus.h>  // Include M5StickC Plus library
+#include <60ghzfalldetection.h>
+#include <SoftwareSerial.h>
+// #include <Wire.h>
+
+#define RX_Pin 32
+#define TX_Pin 33
+
+#ifdef LED_BUILTIN
+#define LED LED_BUILTIN
+#else
+#define LED G10
+#endif
+
+#define REPORTING_PERIOD_MS 1000
+#define BLINK_PERIOD 3000   // milliseconds until cycle repeat
+#define BLINK_DURATION 100  // milliseconds LED is on for
+
+#define MESH_SSID "JoyCheng"
+#define MESH_PASSWORD "heartsensor"
+#define MESH_PORT 5555
+
+SoftwareSerial mySerial(RX_Pin, TX_Pin);
+FallDetection_60GHz radar = FallDetection_60GHz(&mySerial);
+// Prototypes
+void sendMessage();
+void receivedCallback(uint32_t from, String &msg);
+void newConnectionCallback(uint32_t nodeId);
+void changedConnectionCallback();
+void nodeTimeAdjustedCallback(int32_t offset);
+void delayReceivedCallback(uint32_t from, int32_t delay);
+
+
+Scheduler userScheduler;  // to control your personal task
+painlessMesh mesh;
+
+uint32_t tsLastReport = 0;
+uint32_t sensor_report = 0;
+
+bool calc_delay = false;
+SimpleList<uint32_t> nodes;
+SimpleList<String> messageList;
+SimpleList<String> receiveMessage;
+
+String fall_msg = "";
+String exist_msg = "";
+
+void sendMessage();                                                       // Prototype
+Task taskSendMessage(TASK_SECOND * 1, TASK_FOREVER, &sendMessage);        // start with a one second interval
+void printMessages();                                                     // Prototype
+Task taskReceivedMessage(TASK_SECOND * 1, TASK_FOREVER, &printMessages);  // start with a one second interval
+
+// Task to blink the number of nodes
+Task blinkNoNodes;
+bool onFlag = false;
+
+void setup() {
+  delay(5000);
+  Serial.begin(115200);
+  mySerial.begin(115200);
+  M5.begin();
+
+  pinMode(RX_Pin, INPUT);
+  pinMode(TX_Pin, OUTPUT);  
+  pinMode(LED, OUTPUT);
+
+  // Set 'Fall time setting' to 5 seconds
+  byte sensor_cmd1[] = {0x53, 0x59, 0x83, 0x0C, 0x00, 0x04, 0x05, 0x1DB, 0x54, 0x43};
+  mySerial.write(sensor_cmd1, sizeof(sensor_cmd1));
+
+  // Set 'Residence time setting' to 60 seconds
+  byte sensor_cmd2[] = {0x53, 0x59, 0x83, 0x0A, 0x00, 0x04, 0x60, 0x234, 0x54, 0x43};
+  mySerial.write(sensor_cmd2, sizeof(sensor_cmd2));
+
+  // Set 'Fall sensitivity setting' to 3 units
+  byte sensor_cmd3[] = {0x53, 0x59, 0x83, 0x0D, 0x00, 0x04, 0x03, 0x1DA, 0x54, 0x43};
+  mySerial.write(sensor_cmd3, sizeof(sensor_cmd3));
+
+  // Set 'Height accumulation time setting' to 5 seconds
+  byte sensor_cmd4[] = {0x53, 0x59, 0x83, 0x0F, 0x00, 0x04, 0x05, 0x1DE, 0x54, 0x43};
+  mySerial.write(sensor_cmd4, sizeof(sensor_cmd4));
+
+  while (!Serial);
+  if (mySerial.isListening()) { 
+    Serial.println("mySerial is listening!");
+  }
+
+  mesh.setDebugMsgTypes(ERROR | DEBUG);  // set before init() so that you can see error messages
+  mesh.init(MESH_SSID, MESH_PASSWORD, &userScheduler, MESH_PORT);
+  mesh.onReceive(&receivedCallback);
+  mesh.onNewConnection(&newConnectionCallback);
+  mesh.onChangedConnections(&changedConnectionCallback);
+  mesh.onNodeTimeAdjusted(&nodeTimeAdjustedCallback);
+  mesh.onNodeDelayReceived(&delayReceivedCallback);
+  userScheduler.addTask(taskSendMessage);
+  taskSendMessage.enable();
+  userScheduler.addTask(taskReceivedMessage);
+  taskReceivedMessage.enable();
+  blinkNoNodes.set(BLINK_PERIOD, (mesh.getNodeList().size() + 1) * 2, []() {
+    // If on, switch off, else switch on
+    if (onFlag)
+      onFlag = false;
+    else
+      onFlag = true;
+    blinkNoNodes.delay(BLINK_DURATION);
+
+    if (blinkNoNodes.isLastIteration()) {
+      // Finished blinking. Reset task for next run
+      // blink number of nodes (including this node) times
+      blinkNoNodes.setIterations((mesh.getNodeList().size() + 1) * 2);
+      // Calculate delay based on current mesh time and BLINK_PERIOD
+      // This results in blinks between nodes being synced
+      blinkNoNodes.enableDelayed(BLINK_PERIOD - (mesh.getNodeTime() % (BLINK_PERIOD * 1000)) / 1000);
+    }
+  });
+  userScheduler.addTask(blinkNoNodes);
+  blinkNoNodes.enable();
+
+  // jsonDocument = StaticJsonDocument<200>();
+  // array = jsonDocument.to<JsonArray>();
+
+  randomSeed(analogRead(G10));
+}
+
+void fall_detection() {
+  if(sensor_report != 0x00){
+    switch(sensor_report){
+      case NOFALL:
+        fall_msg = "The sensor detects this movement is not a fall.";
+        break;
+      case FALL:
+        fall_msg = "The sensor detects a fall.";
+        break;
+      case NORESIDENT:
+        fall_msg = "The sensors did not detect anyone staying in place.";
+        break;
+      case RESIDENCY:
+        fall_msg = "The sensor detects someone staying in place.";
+        break;
+    }
+  }
+}
+
+void human_existent() {
+  if(radar.sensor_report != 0x00){
+    switch(radar.sensor_report){
+      case NOONE:
+        exist_msg = "Nobody here.";
+        break;
+      case SOMEONE:
+        exist_msg = "Someone is here.";
+        break;
+      case NONEPSE:
+        exist_msg = "No human activity messages.";
+        break;
+      case STATION:
+        exist_msg = "Someone stop";
+        break;
+      case MOVE:
+        exist_msg = "Someone moving";
+        break;
+      case BODYVAL:
+        exist_msg = "The parameters of human body signs are: ";
+        // Serial.println(radar.bodysign_val, DEC);
+        break;
+    }
+  }
+}
+
+void loop() {
+  static unsigned long lastMeshUpdateTime = 0; // Variable to store the last time mesh.update() was called
+  static unsigned long lastFallDetectionTime = 0; // Variable to store the last time fall_detection() was called
+
+  if (!mySerial.isListening()) { 
+    Serial.println("mySerial is not listening!");
+  }
+
+  // Check if it's time to run fall_detection()
+  if (millis() - lastFallDetectionTime >= 100) {
+    radar.Fall_Detection();           //Receive radar data and start processing
+    sensor_report = radar.sensor_report;
+    fall_detection();
+
+    // radar.reset_func();
+
+    // radar.HumanExis_Func();
+    // sensor_report = radar.sensor_report;
+    // human_existent();
+    lastFallDetectionTime = millis(); // Update the last fall_detection() time
+  }
+  
+  // Check if it's time to run mesh.update()
+  if (millis() - lastMeshUpdateTime >= 1000) {
+    mesh.update();
+    digitalWrite(LED, !onFlag);
+    lastMeshUpdateTime = millis(); // Update the last mesh.update() time
+  }
+
+  // delay(1000); //Add time delay to avoid program jam
+}
+
+int randomizePriority() {
+  return random(1, 4);
+}
+
+void receivedCallback(uint32_t from, String &msg) {
+  StaticJsonDocument<200> jsonDocument;
+  DeserializationError error = deserializeJson(jsonDocument, msg);
+  if (error) {
+    Serial.print("Parsing failed: ");
+    Serial.println(error.c_str());
+    return;
+  }
+
+  uint32_t priority = jsonDocument["priority"].as<uint32_t>();
+
+  SimpleList<String>::iterator it = messageList.begin();
+  while (it != messageList.end()) {
+    StaticJsonDocument<200> existingMsg;
+    DeserializationError existingError = deserializeJson(existingMsg, *it);
+    if (existingError) {
+      Serial.print("Parsing failed for existing message: ");
+      Serial.println(existingError.c_str());
+      continue;  // Skip this message
+    }
+
+    uint32_t existingPriority = existingMsg["priority"].as<uint32_t>();
+    if (priority < existingPriority) {
+      break;  // Found the position to insert the message
+    }
+
+    existingMsg["id"] = mesh.getNodeId();
+    String modifiedMsg;
+    serializeJson(existingMsg, modifiedMsg);
+    mesh.sendBroadcast(modifiedMsg);
+    Serial.printf("Sending modified message: %s\n", modifiedMsg.c_str());
+
+    ++it;
+  }
+
+  receiveMessage.insert(it, msg);
+
+  messageList.insert(it, msg);
+}
+
+void printMessages() {
+  Serial.println("Messages received in the last 2 seconds:");
+  for (const auto &msg : messageList) {
+    StaticJsonDocument<200> jsonDocument;
+    DeserializationError error = deserializeJson(jsonDocument, msg);
+    if (error) {
+      Serial.print("Parsing failed: ");
+      Serial.println(error.c_str());
+      continue;
+    }
+
+    uint32_t id = jsonDocument["id"];
+    JsonObject message = jsonDocument["message"];
+
+    // Loop through all the properties in the message object
+    Serial.printf("Received message from node %u: ", id);
+    for (JsonPair property : message) {
+      Serial.printf("%s = %s, ", property.key().c_str(), property.value().as<String>().c_str());
+    }
+    Serial.println();
+
+    uint32_t priority = jsonDocument["priority"];
+
+    // Format and print the message
+    // Serial.printf("Received message from node %u: heart rate = %f, SpO2 = %f, priority = %u\n", id, heartRate, spO2, priority);
+  }
+  // Clear the message list after printing
+  messageList.clear();
+  taskReceivedMessage.setInterval(TASK_SECOND * 2);  // send every 1 second
+}
+
+void sendMessage() {
+  // Create a JSON document containing the heart rate and SpO2 values
+  StaticJsonDocument<200> jsonDocument;
+  String jsonString;
+  JsonObject message = jsonDocument.createNestedObject("message");
+  message["fall_detection"] = fall_msg;
+  message["human_existent"] = exist_msg;
+
+  jsonDocument["id"] = mesh.getNodeId();
+  jsonDocument["priority"] = randomizePriority();
+
+  uint32_t destinationId = 0;  // Default to 0 if node list is empty
+  if (!mesh.getNodeList().empty()) {
+    destinationId = mesh.getNodeList().front();
+  }
+
+  // Convert the JSON document to a string
+  serializeJson(jsonDocument, jsonString);
+
+  // Send the JSON string
+  mesh.sendSingle(destinationId, jsonString);
+
+  // Print the message being sent
+  Serial.printf("Sending message: %s\n", jsonString.c_str());
+
+  // Set the interval for sending messages
+  taskSendMessage.setInterval(TASK_SECOND);  // send every 1 second
+}
+
+void newConnectionCallback(uint32_t nodeId) {
+  // Reset blink task
+  onFlag = false;
+  blinkNoNodes.setIterations((mesh.getNodeList().size() + 1) * 2);
+  blinkNoNodes.enableDelayed(BLINK_PERIOD - (mesh.getNodeTime() % (BLINK_PERIOD * 1000)) / 1000);
+
+  Serial.printf("--> startHere: New Connection, nodeId = %u\n", nodeId);
+  Serial.printf("--> startHere: New Connection, %s\n", mesh.subConnectionJson(true).c_str());
+}
+
+void changedConnectionCallback() {
+  Serial.printf("Changed connections\n");
+  // Reset blink task
+  onFlag = false;
+  blinkNoNodes.setIterations((mesh.getNodeList().size() + 1) * 2);
+  blinkNoNodes.enableDelayed(BLINK_PERIOD - (mesh.getNodeTime() % (BLINK_PERIOD * 1000)) / 1000);
+
+  nodes = mesh.getNodeList();
+
+  Serial.printf("Num nodes: %d\n", nodes.size());
+  Serial.printf("Connection list:");
+
+  SimpleList<uint32_t>::iterator node = nodes.begin();
+  while (node != nodes.end()) {
+    Serial.printf(" %u", *node);
+    node++;
+  }
+  Serial.println();
+  calc_delay = true;
+}
+
+void nodeTimeAdjustedCallback(int32_t offset) {
+  Serial.printf("Adjusted time %u. Offset = %d\n", mesh.getNodeTime(), offset);
+}
+
+void delayReceivedCallback(uint32_t from, int32_t delay) {
+  Serial.printf("Delay to node %u is %d us\n", from, delay);
+}
